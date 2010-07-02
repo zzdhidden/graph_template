79d643292ff0752ddadcbdb1e5115c87dbd276af
hidden <zzdhidden@gmail.com>
2010-07-02 19:02:27 +0800

    Change format method

--- lib/graph_template/graphviz.rb
+++ lib/graph_template/graphviz.rb
@@ -16,15 +16,12 @@ module GraphTemplate
     def render(template, local_assigns)
       source = template.source
       tpl = @view.render(:inline => source, :locals => local_assigns, :layout => false, :type => :erb)
-      return tpl if local_assigns[:erb]
       #@view.controller.response.content_type ||= Mime::PNG
       #@view.controller.headers['Content-Disposition'] = 'inline'
-      format = tpl.scan(/\/\*!format=(.+?)\*\//i)
-      format = (format.size > 0 ? format.last[0] : (@view.params[:format] || :png)).to_sym
+      format = (local_assigns[:format] || @view.params[:format] || :png).to_sym
       #for test
       return tpl if format == :rdot 
       raise(ArgumentError, "Format must in #{FORMATS.inspect}") unless FORMATS.include?(format)
-      #Layout can be set by graph attr.
       gp = IO::popen("dot -q -T#{format}", "w+")
       gp << tpl
       gp.flush
